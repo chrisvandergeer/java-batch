067e5ac4413890f539fbda955ef2e2792f7f482c
Chris van der Geer <chris@MacBook-Air-van-Chris.local>
2020-11-19 16:29:40 +0100

    chunk processor

---
+++ src/main/java/nl/cge/mybatch/control/PersoonUpperCaseItemReader.java
@@ -0,0 +1,22 @@
+package nl.cge.mybatch.control;
+
+import nl.cge.mybatch.entity.Persoon;
+
+import javax.batch.api.chunk.AbstractItemReader;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Named
+public class PersoonUpperCaseItemReader extends AbstractItemReader {
+
+    @PersistenceContext(name = "pu-java-batch")
+    private EntityManager entityManager;
+
+    @Override
+    public Object readItem() throws Exception {
+        return entityManager.createNamedQuery(Persoon.QRY_FIND_UPPERCASE_NAMEN, Persoon.class)
+                .setMaxResults(1)
+                .getResultList().stream().findAny().orElse(null);
+    }
+}
